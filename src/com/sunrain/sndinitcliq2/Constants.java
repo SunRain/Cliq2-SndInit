9ee07238cfbb69ee9ef91fa296155ce74ec53ac4
Wang Guojian <41245110@qq.com>
2011-12-21 22:27:35 +0800

    first commit

---
+++ src/com/sunrain/sndinitcliq2/Constants.java
@@ -0,0 +1,15 @@
+package com.sunrain.sndinitcliq2;
+
+/**
+ * Created by: User: Pedro Veloso Date: 4/11/11 Time: 12:38 PM
+ */
+public interface Constants {
+
+	public static String LOG_TAG = "SndInit";
+
+	public static boolean DEBUGGING = true;
+
+	public static String DOWNLOADED_BM = "bootmenu.tar.gz";
+
+	public static final String DOWNLOAD_URL = "";
+}
---
+++ src/com/sunrain/sndinitcliq2/DownloadHandler.java
@@ -0,0 +1,88 @@
+package com.sunrain.sndinitcliq2;
+
+import android.util.Log;
+import org.apache.http.util.ByteArrayBuffer;
+
+import java.io.*;
+import java.net.URL;
+import java.net.URLConnection;
+
+/**
+ * Created by IntelliJ IDEA. User: pedro Date: 6/1/11 Time: 6:58 PM To change
+ * this template use File | Settings | File Templates.
+ */
+public class DownloadHandler {
+
+	public Boolean getFileFromURL(String fileURL, String targetFielPath) {
+		try {
+			Utils.debugFunc("Will now download from URL: " + fileURL + "to: "
+					+ targetFielPath, Log.VERBOSE);
+
+			URL url = new URL(fileURL);// new URL(Constants.ROM_SERVER_URL+
+										// fileURL);
+			File file = new File(targetFielPath);
+
+			/* Open a connection to that URL */
+			URLConnection ucon = url.openConnection();
+
+			/*
+			 * Define InputStreams to read from the URLConnection
+			 */
+			InputStream is = ucon.getInputStream();
+			BufferedInputStream bis = new BufferedInputStream(is);
+
+			/*
+			 * Read bytes to the Buffer until there is nothing more to read(-1)
+			 */
+			ByteArrayBuffer baf = new ByteArrayBuffer(50);
+			int current;
+			while ((current = bis.read()) != -1) {
+				baf.append((byte) current);
+			}
+
+			/* Convert the Bytes read to a String */
+			FileOutputStream fos = new FileOutputStream(file);
+			fos.write(baf.toByteArray());
+			fos.close();
+			return true;
+
+		} catch (IOException e) {
+			Utils.debugFunc("Download error.", Log.ERROR);
+			e.printStackTrace();
+			return false;
+		}
+
+	}
+
+	public static String getStringFromURL(String stringURL) {
+		try {
+			URL url = new URL(stringURL);
+
+			/* Open a connection to that URL */
+			URLConnection ucon = url.openConnection();
+
+			/*
+			 * Define InputStreams to read from the URLConnection
+			 */
+			InputStream is = ucon.getInputStream();
+			BufferedInputStream bis = new BufferedInputStream(is);
+
+			/*
+			 * Read bytes to the Buffer until there is nothing more to read(-1)
+			 */
+			ByteArrayBuffer baf = new ByteArrayBuffer(50);
+			int current;
+			while ((current = bis.read()) != -1) {
+				baf.append((byte) current);
+			}
+
+			return new String(baf.toByteArray());
+
+		} catch (IOException e) {
+			Utils.debugFunc("Download error.", Log.ERROR);
+			e.printStackTrace();
+		}
+		return "";
+	}
+
+}
